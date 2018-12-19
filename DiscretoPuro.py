9c4832ed844867fcbbd7116997b96a80ff4a19d7
Leandro Demarco <leandro.demarco@webee.io>
2018-12-18 21:52:21 -0300

    Fixed DiscretoPuro logarithmic

--- DiscretoPuro.py
+++ DiscretoPuro.py
@@ -11,8 +11,7 @@ Corre la versión discreta pura del algoritmo
 import ACOR
 import Utils
 
-use_log = False # Change this flag to use logarithmic version
-best_r1_only = False
+use_log = True # Change this flag to use logarithmic version
 
 output_dir = ('DiscretoPuro/Logaritmico/' if use_log 
               else 'DiscretoPuro/Comun/')
@@ -21,23 +20,29 @@ f = open(output_dir + 'results.txt', 'w+')
 a = ACOR.Acor('DiscretoPuro', use_log)
 u = Utils.Utils()
 
-iterations_per_rval = 20
-r1_vals = u.best_r1_sens if best_r1_only else u.res_vals
+iterations_per_rval = 100
+r1_vals = u.r1_with_sols
 for R1 in r1_vals:
-    done = False
+    found_sols = []
+    iterations_with_sol = 0
     for i in range(0, iterations_per_rval):
         print 'Running iter %i for R1: %i' % (i+1, R1)
-        if done:
-            break
-        best_sol = a.main_loop(R1)
+        best_sol = a.main_loop(R1)[:-1]
+        if (use_log):
+            best_sol = Utils.exp_list(best_sol)
         r2, r3, c4, c5 = best_sol[0], best_sol[1], best_sol[2], best_sol[3]
-        sens = u.get_sol_info(R1, r2, r3, c4, c5)[0]
-        isSol = u.is_sol(R1, r2, r3, c4, c5)
+        sol = [R1, r2, r3, c4, c5]
+        sens = u.get_sol_info_grouped(sol)[0]
+        isSol = u.is_sol_grouped(sol)
         if (isSol):
-            done = True
-            f.write('Iteraciones: ' +str(i+1))
-            f.write(u.full_solution_string(R1, r2, r3, c4, c5))
-    if not done:
+            iterations_with_sol += 1
+            if (not sol in found_sols):
+                found_sols.append(sol)
+    if iterations_with_sol == 0:
         f.write('No hallo soluciones para R1: ' + str(R1) + '\n')
+    else:
+        f.write('Hallo %i (%i) soluciones: %s\n' % (len(found_sols), \
+                                                   iterations_with_sol, \
+                                                   str(found_sols)))
             
 f.close()
